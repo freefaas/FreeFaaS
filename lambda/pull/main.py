33262f5e85fd6a767efdca4eed183183b18371ae
Shine <ShineSmile@outlook.com>
2018-04-07 19:55:32 +0800

    feat($aws-lambda-pull): fix output path error.

--- lambda/pull/main.py
+++ lambda/pull/main.py
@@ -5,6 +5,7 @@ import os
 import requests
 import zipfile
 import StringIO
+import time
 
 
 def create_folder_if_not_exists(path):
@@ -16,35 +17,39 @@ def download_zip_and_extractall_to_folder(url, folder):
     content = requests.get(url, stream=True).content
     archive = zipfile.ZipFile(StringIO.StringIO(content))
     archive.extractall(folder)
-    print
 
 
 def fetch_lambda(name, target_folder):
-    response = client.get_function(FunctionName=name)
-
-    config = response['Configuration']
-    config_path = os.path.abspath(os.path.join(target_folder, 'config.dict'))
-    # save configuration to file
-    config_file = open(config_path, 'w+')
-    config_file.write(str(config))
-    config_file.flush()
-    config_file.close()
-
-    code_path = os.path.abspath(os.path.join(target_folder, 'Code'))
-    create_folder_if_not_exists(code_path)
-    # save code to files
-    code = response['Code']
-    repository_type = code['RepositoryType']
-    location = code['Location']
-
-    if repository_type == 'S3':
-        download_zip_and_extractall_to_folder(location, code_path)
+    for i in range(3):
+        try:
+            response = client.get_function(FunctionName=name)
+            # fetch config
+            config_path = os.path.abspath(os.path.join(target_folder, 'config.dict'))
+            config = response['Configuration']
+            config_file = open(config_path, 'w+')
+            config_file.write(str(config))
+            config_file.flush()
+            config_file.close()
+            # fetch code
+            code_path = os.path.abspath(os.path.join(target_folder, 'Code'))
+            create_folder_if_not_exists(code_path)
+            code = response['Code']
+            repository_type = code['RepositoryType']
+            location = code['Location']
+            if repository_type == 'S3':
+                download_zip_and_extractall_to_folder(location, code_path)
+        except requests.exceptions.RequestException:
+            t = 3 ** i
+            print(str.format('{0} request error {1} times. Wait {2} seconds to restart.', name, i, t))
+            time.sleep(3 ** i)
+            continue
+        break
 
 
 # the output folder will be the git local working space
 # delete the folder manually if necessary
-output_folder = os.path.abspath(os.path.join('../../assets'))
-print output_folder
+output_folder = os.path.abspath(os.path.join('assets/aws_lambda'))
+print(output_folder)
 
 client = boto3.client('lambda')
 functions = client.list_functions()['Functions']
@@ -63,23 +68,17 @@ for func in functions:
         versionArn = version['FunctionArn']
         # make version folder
         versionPath = os.path.abspath(os.path.join(versionsPath, version['Version']))
-        print versionPath
+        print(versionPath)
         create_folder_if_not_exists(versionPath)
         fetch_lambda(versionArn, versionPath)
-        # may be useful in future
-        # tags = response['Tags']
-        # concurrency = response['Concurrency']
     # dimensionality of aliases
     aliases = client.list_aliases(FunctionName=functionName)['Aliases']
-    aliasesPath = os.path.abspath(os.path.join(functionPath, 'Aliases'))
+    aliasesPath = os.path.abspath(os.path.join(functionPath, 'aliases'))
     create_folder_if_not_exists(aliasesPath)
     for alias in aliases:
         aliasArn = alias['AliasArn']
         # make alias folder
         aliasPath = os.path.abspath(os.path.join(aliasesPath, alias['Name']))
-        print aliasPath
+        print(aliasPath)
         create_folder_if_not_exists(aliasPath)
         fetch_lambda(aliasArn, aliasPath)
-    # may be useful in future
-    # response = client.list_event_source_mappings(FunctionName=functionName)
-    # print(response)
